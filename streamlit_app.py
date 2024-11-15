cf2cab650aed3edb20bb0d2b156ed1240b0431ca
Tom Greeven <tom.greeven@zijm-bi.nl>
2024-11-15 12:10:40 +0100

    aanpassingen

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,35 +6,37 @@ st.title("Afbeelding downloader")
 # Invoerveld voor de URL
 url = st.text_input("Geef de URL van VW configurator op:")
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
-    from webdriver_manager.core.os_manager import ChromeType
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
 
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(
-            service=Service(
-                ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
-            ),
-            options=options,
-        )
-
+@st.cache_resource
+def get_driver():
     options = Options()
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
 
-    # Knop om het downloaden te starten
-    if st.button("Download Afbeeldingen"):
-        if url:
-            st.write("Downloadproces gestart...")
-            
-            driver = get_driver()
-            driver.get(url)
+    return webdriver.Chrome(
+        service=Service(
+            ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+        ),
+        options=options,
+    )
+
+# Knop om het downloaden te starten
+if st.button("Download Afbeeldingen"):
+    if url:
+        st.write("Downloadproces gestart...")
         
-            st.code(driver.page_source)
-
+        driver = get_driver()
+        driver.get(url)
+    
+        # Toon de HTML van de pagina
+        st.code(driver.page_source)
+        driver.quit()
     else:
         st.warning("Voer een geldige URL in voordat je op 'Download Afbeeldingen' klikt.")
